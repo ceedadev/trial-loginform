44e0ed4ee1eeb04943aacaa62aec24fcc20e1c61
ceedadev <ceedadev@gmail.com>
2024-03-25 21:52:25 +0700

    declare assets and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
+const apercu = localFont({
+  src: "../../public/fonts/Apercu Pro.woff",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`antialiased ${apercu.className}`}>{children}</body>
     </html>
   );
 }
